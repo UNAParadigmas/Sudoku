e448aed5b0ea7897109b2034301c5c61e6330e9a
Luis Fernando Vásquez <luisf96v@gmail.com>
2017-08-21 21:35:23 -0600

    Cambios

---
+++ Sudoku App/js/Tipe Scripts/Matrix.ts
@@ -0,0 +1,14 @@
+class Matrix {
+	matrix: string;
+	boardSize: number;
+	
+	constructor(size: number, mat: string){
+		this.boardSize = size;
+		this.matrix = mat;
+	}
+	
+	compare(mat: string){
+		
+	}
+	
+}
