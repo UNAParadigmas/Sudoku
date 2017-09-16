13fab6919a5e438c0e1d8d7cf94ad8af6c33f17d
Denis <vite0150@gmail.com>
2017-09-15 19:47:43 -0600

    verificacion de registro verificar si ya existe un usuario con ese username

--- routes/registro.js
+++ routes/registro.js
@@ -7,30 +7,43 @@ const Usuario = require("../model/usuario");
 
 //registro usuario
 //ejemplo ARC= http://localhost:8080/api/registro
-//{"nombre" : "Juan","usuario" : "vite0150","pass" : "123"}
+//{"nombre" : "Juan","user" : "vite0150","pass" : "123"}
 router.post('/registro', function (req, res, next) {
     let data = req.body;
-    console.log("registro de usuario, nombre: ",data.nombre);
-    
+    console.log("registro de usuario, nombre: ", data.nombre);
 
-    let newuser= new Usuario({
+
+    let newuser = new Usuario({
         nombre: data.nombre,
         usuario: data.user,
         pass: data.pass
     });
 
-    
-
-    newuser.save((err, usuario) => {
-        if (err){
-            console.log(err);
-            res.send(err);
+    Usuario.findOne({ usuario: newuser.usuario }, (err, usuario) => {
+        if (err) {
+            res.status(500).send(err);
         }
-        else {
-            console.log("usuario creado: ",usuario);
-            res.status(200).json(usuario);
+        if (usuario) {//ya existe ese usuario
+            console.log("Error al registrar, el usuario: ", usuario.usuario+" ya EXISTE");
+            res.status(500).send(null);
+        }
+        else {//el usuario se puede registrar
+
+            newuser.save((err, usuario) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+                else {
+                    console.log("usuario creado: ", usuario);
+                    res.status(200).json(usuario);
+                }
+            });
+
         }
     });
+
+
 });
 
 
