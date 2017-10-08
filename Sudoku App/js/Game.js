4e913206eb36a2eb71ec9c02d0b0596d11517687
Luis Fernando Vásquez <luisf96v@gmail.com>
2017-10-08 17:36:06 -0600

    Game.js Functional and O(n) Optimization

--- Sudoku App/js/Game.js
+++ Sudoku App/js/Game.js
@@ -1,6 +1,6 @@
 
 class Game {
-		constructor(showAllowed = true) {
+		constructor(showSingles = true, showAllowed = true) {
 			this.CellSize = 60;
 			this.SubCellSize = 18;
 			this.canvas = document.getElementById("canvas");
@@ -9,9 +9,87 @@ class Game {
 			this.selCol = 0;
 			this.selRowBef = 0;
 			this.selColBef = 0;
-			this.showAllowed = showAllowed;
 			this.stack = new Array();
+			
+			this.showSingles = showSingles; // print singles in red
+			this.showAllowed = showAllowed; // print the mask in each cell
 		}
+		
+		/* UI methods */
+		
+		drawGrid() { //use canvas to draw the sudoku grid
+			let drawPath = function(i){
+				let val = i * 60 + 0.5;
+				con.beginPath();
+				con.lineWidth = (i%3) ? 1 : 3;
+				//verticals lines
+				con.moveTo(val, 0.5);
+				con.lineTo(val, dim);
+				//horizontal lines
+				con.moveTo(0.5 , val);
+				con.lineTo(dim, val);
+				con.stroke();
+			}	
+			let con = this.canvas.getContext('2d'); 
+			con.strokeStyle = '#808080';
+			
+			let dim = 540.5; // dim = 9(boardSize) * 60(cellSize) + 0.5(space) 
+							
+			Array.from({length:10}).forEach((e,i)=>drawPath(i));			
+		}
+		
+		drawSingleCell(con, loc, selVal){
+			let cel = this.board.getCell(loc);
+				
+			if (this.showAllowed && cel.isNotAssigned()) { //Draw allowed
+				con.font = "12pt Calibri"; 
+				cel.mask.valuesArray().forEach ( val => {//Show all possible values that can be in that cell
+					let subCol = (loc.col + 0.5) * 60 + (Math.floor((val - 1) % 3) - 1) * 18;
+					let subRow = (loc.row + 0.5) * 60 + (Math.floor((val - 1) / 3) - 1) * 18;
+					
+					if (this.showSingles && cel.mask.count == 1) con.fillStyle = "#ff143c"; //print the single in red
+					else con.fillStyle = "#aaaaaa";
+					
+					con.fillText(val, subCol, subRow);
+				});
+			}else if(!!cel.getValue()){ // draw values
+				let value = cel.getValue();
+				con.font = "32pt Calibri";
+				let subCol = (loc.col + 0.5) * 60;
+				let subRow = (loc.row + 0.5) * 60;
+				
+				if(!!selVal && value == selVal){// same number than selected row-col
+					con.fillStyle = "#F91919";
+				}else{
+					con.fillStyle = cel.isGiven() ? "#2200aa" : "#696969"; 
+				}
+				con.fillText(cel.getValue(), subCol, subRow);
+			}
+		}
+		
+		drawCells() {// updates every cell with the value or the mask
+			let con = this.canvas.getContext('2d'); 
+			con.textAlign = "center";
+			con.textBaseline = "middle";
+			
+			//print selected
+			con.beginPath();
+			con.rect(this.selCol * 60 + 2.5, this.selRow * 60 + 2.5, 56, 56);
+			con.fillStyle = "#A9F5E1";
+			con.fill();	
+			
+			let selValue = this.board.getCell(new Location(this.selRow, this.selCol)).getValue();
+			this.board.locs.forEach(loc => this.drawSingleCell(con,loc,selValue));			
+		}
+		
+		updateCanvas() {
+			this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+			this.drawGrid();
+			this.drawCells();
+			//message.innerHTML = (!this.board._isValid)? "Incorrecto" : (this.board._isSolved)? "Resuelto" : "";
+		}
+		
+		
 		/* stack methods */
 		undo () {
 			if (this.stack.length) {
@@ -24,89 +102,7 @@ class Game {
 			if(!this.beforeValEquals())
 				this.stack.push(this.board.getString());
 		}
-		/* UI methods */
-		drawGrid() { 
-			let con = this.canvas.getContext('2d'); 
-			con.strokeStyle = '#808080';
-			
-			for (var i = 0; i <= this.board.size; i++) {
-				let val1 = i * this.CellSize + 0.5;
-				let val2 = this.board.size * this.CellSize + 0.5; 				
-				
-				con.beginPath();
-				con.lineWidth = (i%3) ? 1 : 2;
-				con.moveTo(val1, 0.5 );
-				con.lineTo(val1, val2);
-				con.moveTo(0.5 , val1);
-				con.lineTo(val2, val1);
-				con.stroke();
-			}
-		}
 		
-		drawCells() { //Nota : cambiar  los isNotAssigned
-			let con = this.canvas.getContext('2d'); 
-			con.font = "12pt Calibri"; 
-			con.textAlign = "center";
-			con.textBaseline = "middle";
-			con.beginPath();
-			con.rect(this.selCol * this.CellSize + 2.5, this.selRow * this.CellSize + 2.5, this.CellSize - 4, this.CellSize - 4);
-			con.fillStyle = "#A9F5E1";
-			con.fill();	
-			con.fillStyle = "#999999"; 
-
-			if (this.showAllowed){
-				for (var row = 0; row < this.board.size; row++){
-					for (var col = 0; col < this.board.size; col++) {
-						let cel = this.board.getCell(new Location(row, col));
-						if (cel.isNotAssigned()) { 
-							var values = cel.mask.valuesArray();
-							
-							values.forEach ( (e) => {
-								let x = (col + 0.5) * this.CellSize; 
-								let y = (row + 0.5) * this.CellSize;
-								let subRow = Math.floor((e - 1) / 3) - 1;
-								let subCol = Math.floor((e - 1) % 3) - 1;
-								x += subCol * this.SubCellSize;
-								y += subRow * this.SubCellSize;
-								con.fillStyle = "#aaaaaa"
-								if (values.length == 1 && e == cel.getAnswer())
-									con.fillStyle = "#ff143c";
-								con.fillText(e, x, y);
-							});
-						}
-					}
-				}
-			}		
-			let selectValue = this.board.getCell(new Location(this.selRow, this.selCol)).getValue();
-
-			con.font = "32pt Calibri";
-			con.textAlign = "center";
-			con.textBaseline = "middle";
-			con.fillStyle = "#191929";
-			
-			for (var row = 0; row < this.board.size; row++)
-				for (var col = 0; col < this.board.size; col++) {
-					let cel = this.board.getCell(new Location(row, col));
-					let x = (col + 0.5) * this.CellSize; 
-					let y = (row + 0.5) * this.CellSize;
-					let sameDigit = cel.getValue() == selectValue && selectValue != 0;
-					let value = cel.getValue();
-					if (value != 0) {
-						con.fillStyle = cel.isGiven() ? "#2200aa" : "#696969"; 
-						if (sameDigit)
-							con.fillStyle = "#F91919";
-						con.fillText(value, x, y);
-					}
-				}
-			
-		}
-		
-		updateCanvas() {
-			this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
-			this.drawGrid();
-			this.drawCells();
-			//message.innerHTML = (!this.board._isValid)? "Incorrecto" : (this.board._isSolved)? "Resuelto" : "";
-		}
 		selectCell(row, col) {
 			this.updateBefore()
 			this.selRow = row;
@@ -174,7 +170,7 @@ class Game {
 			let startTime = new Date().getTime();
 			this.pushBoard();
 			if(this.board.trySolve()){
-				let totalTime = ((new Date()).getTime - startTime.getTim)/1000
+				let totalTime = ((new Date()).getTime() - startTime)/1000
 				console.log('solved, time: '+totalTime+ 'seconds.');
 			}else{
 				//dont have answer
